c67e1d542a5b56261cb239f38d397489c059c765
chulihui <chulihui@nibirutech.com>
2023-08-11 11:02:10 +0800

    feat: 时间与字符串转换

---
+++ cmd/time.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/yingzixiaoyuan/tools/internal/timer"
+)
+
+var calculateTime string
+var duration string
+var tamp int64
+
+var timeCmd = &cobra.Command{
+	Use:   "time",
+	Short: "时间格式处理",
+	Long:  "时间格式处理",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+var nowTimeCmd = &cobra.Command{
+	Use:   "now",
+	Short: "获取当前时间",
+	Long:  "获取当前时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		nowTime := timer.GetNowTime()
+		log.Printf("输出结果: %s, %d", nowTime.Format("2006-01-02 15:04:05"), nowTime.Unix())
+	},
+}
+
+var tampCmd = &cobra.Command{
+	Use:   "tamp",
+	Short: "将时间戳转换成本地时间",
+	Long:  "将时间戳转换成本地时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		nowTime := timer.ConvertStampToTime(tamp)
+		log.Printf("输出结果: %d, %s", nowTime.Unix(), nowTime.Format("2006-01-02 15:04:05"))
+	},
+}
+
+var calculateTimeCmd = &cobra.Command{
+	Use:   "calc",
+	Short: "计算所需时间",
+	Long:  "计算所需时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		var currentTimer time.Time
+		var layout = "2006-01-02 15:04:05"
+		if calculateTime == "" {
+			currentTimer = timer.GetNowTime()
+		} else {
+			var err error
+			space := strings.Count(calculateTime, " ")
+			if space == 0 {
+				layout = "2006-01-02"
+			}
+			if space == 1 {
+				layout = "2006-01-02 15:04"
+			}
+			currentTimer, err = time.Parse(layout, calculateTime)
+			if err != nil {
+				t, _ := strconv.Atoi(calculateTime)
+				currentTimer = time.Unix(int64(t), 0)
+			}
+		}
+		t, err := timer.GetCalculateTime(currentTimer, duration)
+		if err != nil {
+			log.Fatalf("timer.GetCalculateTime err: %v", err)
+		}
+
+		log.Printf("输出结果: %s, %d", t.Format(layout), t.Unix())
+	},
+}
+
+func init() {
+	timeCmd.AddCommand(nowTimeCmd)
+	timeCmd.AddCommand(calculateTimeCmd)
+	timeCmd.AddCommand(tampCmd)
+
+	calculateTimeCmd.Flags().StringVarP(&calculateTime, "calculate", "c", "", ` 需要计算的时间，有效单位为时间戳或已格式化后的时间 `)
+	calculateTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", ` 持续时间，有效时间单位为"ns", "us" (or "µ s"), "ms", "s", "m", "h"`)
+	tampCmd.Flags().Int64VarP(&tamp, "tamp", "t", 0, ` 时间戳,将时间戳转换成本地时间`)
+}
---
+++ internal/timer/time.go
@@ -0,0 +1,20 @@
+package timer
+
+import "time"
+
+func GetNowTime() time.Time {
+	return time.Now()
+}
+
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	duration, err := time.ParseDuration(d)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return currentTimer.Add(duration), nil
+}
+
+func ConvertStampToTime(timestamp int64) time.Time {
+	return time.Unix(timestamp, 0)
+}
